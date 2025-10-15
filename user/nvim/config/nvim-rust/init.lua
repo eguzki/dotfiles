47ef17c882b968908d1ef049f1f0a695be6b9f2c
Eguzki Astiz Lezaun <eastizle@redhat.com>
2025-10-15 20:31:09 +0200

    treesitter for rust
    Signed-off-by: Eguzki Astiz Lezaun <eastizle@redhat.com>

--- user/nvim/config/nvim-rust/init.lua
+++ user/nvim/config/nvim-rust/init.lua
@@ -851,17 +851,14 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'rust', 'diff', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'toml', 'yaml'},
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
         enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
+        additional_vim_regex_highlighting = false,
       },
-      indent = { enable = true, disable = { 'ruby' } },
+      indent = { enable = true },
     },
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
