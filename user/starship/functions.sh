349197fe981d7e366e93a7be0ada5a89ad90d121
Eguzki Astiz Lezaun <eastizle@redhat.com>
2025-10-22 18:51:07 +0200

    starship config file
    Signed-off-by: Eguzki Astiz Lezaun <eastizle@redhat.com>

--- user/starship/functions.sh
+++ user/starship/functions.sh
@@ -2,9 +2,15 @@ install_starship()  {
   # https://starship.rs/
 
   local INSTALL_DIR
+  local LOCAL_FILE
+  local LOCAL_DIR
+  local DEST_FILE
 
   # --- Configuration ---
   INSTALL_DIR="$HOME/apps/starship"
+  LOCAL_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+  LOCAL_FILE="${LOCAL_DIR}/starship.toml"
+  DEST_FILE="$HOME/.config/starship.toml"
 
   if [ -d "$INSTALL_DIR" ]; then
     _logInfo "  Found existing starship installation at $INSTALL_DIR"
@@ -29,5 +35,10 @@ install_starship()  {
 export PATH=$INSTALL_DIR:\$PATH
 eval "\$(starship init bash)"
 EOF
+
+  # 3. Add starship configuration
+  #
+  cp -r $LOCAL_FILE $DEST_FILE
+
   _logInfo "  ✅ starship installed successfully to $INSTALL_DIR"
 }
