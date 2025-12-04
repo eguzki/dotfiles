dfe16b9ab3dd5ba81552463d9278cb105dde1e48
Eguzki Astiz Lezaun <eastizle@redhat.com>
2025-12-04 10:59:41 +0100

    search visual selection
    Signed-off-by: Eguzki Astiz Lezaun <eastizle@redhat.com>

--- user/nvim/config/nvim-ts/init.lua
+++ user/nvim/config/nvim-ts/init.lua
@@ -365,6 +365,7 @@ require("lazy").setup({
 			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
 			vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
 			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+			vim.keymap.set("v", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
 			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
 			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
@@ -717,7 +718,7 @@ require("lazy").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
 				-- You can customize some of the format options for the filetype (:help conform.format)
-				typescript = { "biome", "biome-organize-imports"  },
+				typescript = { "biome", "biome-organize-imports" },
 			},
 		},
 	},
