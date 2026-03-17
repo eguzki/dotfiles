bee19be524157dfcffd73248646e398c139aa6d9
Eguzki Astiz Lezaun <eastizle@redhat.com>
2026-03-17 16:21:23 +0100

    fix nvim-treesitter
    Signed-off-by: Eguzki Astiz Lezaun <eastizle@redhat.com>

---
+++ user/nvim/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,50 @@
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local treesitter = require("nvim-treesitter.configs")
+
+			-- configure treesitter
+			treesitter.setup({ -- enable syntax highlighting
+				highlight = {
+					enable = true,
+				},
+				-- enable indentation
+				indent = { enable = true },
+				-- ensure these language parsers are installed
+				ensure_installed = {
+					"bash",
+					"go",
+					"diff",
+					"rust",
+					"json",
+					"javascript",
+					"typescript",
+					"tsx",
+					"toml",
+					"yaml",
+					"html",
+					"css",
+					"markdown",
+					"markdown_inline",
+					"bash",
+					"lua",
+					"vim",
+					"dockerfile",
+					"gitignore",
+				},
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<C-space>",
+						node_incremental = "<C-space>",
+						scope_incremental = false,
+						node_decremental = "<bs>",
+					},
+				},
+			})
+		end,
+	},
+}
