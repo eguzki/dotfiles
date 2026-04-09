d6bb3e8dc8ad189aa0b80b28ddaac04c5a6ce0f2
Eguzki Astiz Lezaun <eastizle@redhat.com>
2026-04-09 11:51:48 +0200

    go1.25.8
    Signed-off-by: Eguzki Astiz Lezaun <eastizle@redhat.com>

--- user/go/functions.sh
+++ user/go/functions.sh
@@ -1,45 +1,45 @@
-install_go()  {
+install_go() {
 
-  local VERSION
-  local INSTALL_DIR
-  local DOWNLOAD_URL
-  local TMP_DIR
-  local TMP_FILE
+	local VERSION
+	local INSTALL_DIR
+	local DOWNLOAD_URL
+	local TMP_DIR
+	local TMP_FILE
 
-  # --- Configuration ---
-  VERSION="1.25.6"
-  INSTALL_DIR="$HOME/apps/go-${VERSION}"
-  DOWNLOAD_URL="https://go.dev/dl/go${VERSION}.linux-amd64.tar.gz"
+	# --- Configuration ---
+	VERSION="1.25.8"
+	INSTALL_DIR="$HOME/apps/go-${VERSION}"
+	DOWNLOAD_URL="https://go.dev/dl/go${VERSION}.linux-amd64.tar.gz"
 
-  if [ -d "$INSTALL_DIR" ]; then
-    _logInfo "  Found existing go installation at $INSTALL_DIR"
-    return
-  fi
+	if [ -d "$INSTALL_DIR" ]; then
+		_logInfo "  Found existing go installation at $INSTALL_DIR"
+		return
+	fi
 
-  # This script downloads and installs the release of go
-  # for Linux x86-64 into the user's home directory (~/apps/go-x.y.z).
+	# This script downloads and installs the release of go
+	# for Linux x86-64 into the user's home directory (~/apps/go-x.y.z).
 
-  # --- Main Logic ---
+	# --- Main Logic ---
 
-  # Create a temporary directory
-  TMP_DIR=$(mktemp -d)
-  TMP_FILE="${TMP_DIR}/go-linux-amd64.tar.gz"
+	# Create a temporary directory
+	TMP_DIR=$(mktemp -d)
+	TMP_FILE="${TMP_DIR}/go-linux-amd64.tar.gz"
 
-  # 1. Download the Neovim release
-  curl -L "$DOWNLOAD_URL" -o "$TMP_FILE"
+	# 1. Download the Neovim release
+	curl -L "$DOWNLOAD_URL" -o "$TMP_FILE"
 
-  # 2. Extract the downloaded archive into the home directory
-  mkdir -p "$INSTALL_DIR"
-  tar xzvf "$TMP_FILE" -C "$INSTALL_DIR" --strip-components=1
+	# 2. Extract the downloaded archive into the home directory
+	mkdir -p "$INSTALL_DIR"
+	tar xzvf "$TMP_FILE" -C "$INSTALL_DIR" --strip-components=1
 
-  # 3. Clean up the temporary file
-  rm -rf "$TMP_DIR"
+	# 3. Clean up the temporary file
+	rm -rf "$TMP_DIR"
 
-  # 4. Add go to the system's PATH
-  #
-  cat <<EOF > ~/.bashrc.d/go
+	# 4. Add go to the system's PATH
+	#
+	cat <<EOF >~/.bashrc.d/go
 # Go
 export PATH=$INSTALL_DIR/bin:\$PATH
 EOF
-  _logInfo "  ✅ go installed successfully to $INSTALL_DIR"
+	_logInfo "  ✅ go installed successfully to $INSTALL_DIR"
 }
