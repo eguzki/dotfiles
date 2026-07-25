c28c12f6d5249cd06490b3dea515ad28ce38792b
Eguzki Astiz Lezaun <eastizle@redhat.com>
2026-07-25 18:24:35 +0200

    Fix Treesitter folding by switching to native foldexpr
    Signed-off-by: Eguzki Astiz Lezaun <eastizle@redhat.com>

--- user/nvim/nvim/lua/config/options.lua
+++ user/nvim/nvim/lua/config/options.lua
@@ -94,5 +94,5 @@ vim.o.confirm = true
 
 --- Enable folding via Treesitter
 vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldlevel = 99
